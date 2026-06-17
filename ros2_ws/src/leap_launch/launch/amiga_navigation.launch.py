6fac2ca079c8a88135c20f4baf7fac5fbf11dd6d
Atharv Goel <atharvgo@andrew.cmu.edu>
2026-06-17 10:39:05 -0400

    some very basic nav2 stuff

--- ros2_ws/src/leap_launch/launch/amiga_navigation.launch.py
+++ ros2_ws/src/leap_launch/launch/amiga_navigation.launch.py
@@ -2,11 +2,16 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, GroupAction 
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node, SetRemap
 
 def generate_launch_description():
     pkg_leap_control = get_package_share_directory('leap_control')
     map_yaml_path = os.path.join(pkg_leap_control, 'maps', 'cmu_occ.yaml')
+
+    leap_nav = get_package_share_directory('leap_nav')
+    nav2_launch_path = os.path.join(leap_nav, 'launch', 'navigation_launch.py')
 
     nav2_server_node = Node(
         package='nav2_map_server',
@@ -24,9 +29,23 @@ def generate_launch_description():
         name='lifecycle_manager_map_server',
         output='screen',
         parameters=[
-            {'use_sim_time': True},
             {'autostart': True},
             {'node_names': ['map_server']}
+        ]
+    )
+
+    nav2_navigation_group = GroupAction(
+        actions=[
+            # Catch the default /cmd_vel and push it to /cmd_vel_nav2 (/cmd_vel_nav is an internal nav2 topic)
+            SetRemap(src='/cmd_vel', dst='/cmd_vel_nav2'),
+            
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(nav2_launch_path),
+                # launch_arguments={
+                #     # 'params_file': nav2_params_path
+                #     'use_velocity_smoother': 'false'
+                # }.items()
+            )
         ]
     )
 
@@ -34,5 +53,6 @@ def generate_launch_description():
 
     ld.add_action(nav2_server_node)
     ld.add_action(nav2_manager_node)
+    ld.add_action(nav2_navigation_group)
 
     return ld
